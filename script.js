eb8cf6c00c88cc39e3f854fb2912016f84e0cfeb
Anupom Das <anudasrpmc39@gmail.com>
2026-04-08 00:51:52 +0200

    Add language toggle functionality

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let isEnglish = true;
 
+function toggleLang() {
+  isEnglish = !isEnglish;
+
+  document.querySelectorAll("[data-en]").forEach(el => {
+    el.innerText = isEnglish ? el.dataset.en : el.dataset.bn;
+  });
+}
